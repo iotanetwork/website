d2b78243f05ba11080bf7ccf2337ea9765b31dbb
Kumar Anirudha <anirudhastark@yahoo.com>
2018-01-06 18:43:00 +0530

    map changed. added upto 18 row.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -137,3 +137,192 @@ jQuery(function($) {
     });
   });
   });
+
+
+  /**
+   * This example uses pulsating circles CSS by Kevin Urrutia
+   * http://kevinurrutia.tumblr.com/post/16411271583/creating-a-css3-pulsating-circle
+   */
+
+  var map = AmCharts.makeChart( "chartdiv", {
+    "type": "map",
+    "theme": "light",
+    "projection": "miller",
+
+    "imagesSettings": {
+      "rollOverColor": "#089282",
+      "rollOverScale": 3,
+      "selectedScale": 3,
+      "selectedColor": "#089282",
+      "color": "#13564e"
+    },
+
+    "areasSettings": {
+      "unlistedAreasColor": "#15A892"
+    },
+
+    "dataProvider": {
+      "map": "worldLow",
+      "images": [
+          {
+            "zoomLevel": 5,
+            "scale": 0.5,
+            "title": "New York",
+            "latitude": 40.7128,
+            "longitude": -74.0060
+          }, {
+            "zoomLevel": 5,
+            "scale": 0.5,
+            "title": "Montrel",
+            "latitude": 45.5017,
+            "longitude": -73.5673
+          }, {
+            "zoomLevel": 5,
+            "scale": 0.5,
+            "title": "Paris",
+            "latitude": 48.8567,
+            "longitude": 2.3510
+          }, {
+            "zoomLevel": 5,
+            "scale": 0.5,
+            "title": "Hong Kong",
+            "latitude": 22.3964,
+            "longitude": 114.1095
+          }, {
+            "zoomLevel": 5,
+            "scale": 0.5,
+            "title": "Bangalore",
+            "latitude": 12.9716,
+            "longitude": 77.5946
+          }, {
+            "zoomLevel": 5,
+            "scale": 0.5,
+            "title": "Berlin",
+            "latitude": 52.5200,
+            "longitude": 13.4050
+          }, {
+            "zoomLevel": 5,
+            "scale": 0.5,
+            "title": "Rome",
+            "latitude": 41.9028,
+            "longitude": 12.4964
+          }, {
+            "zoomLevel": 5,
+            "scale": 0.5,
+            "title": "London",
+            "latitude": 51.5002,
+            "longitude": -0.1262
+          }, {
+            "zoomLevel": 5,
+            "scale": 0.5,
+            "title": "Washington DC",
+            "latitude": 38.8921,
+            "longitude": -77.0241
+          }, {
+            "zoomLevel": 5,
+            "scale": 0.5,
+            "title": "New Orleans",
+            "latitude": 29.9511,
+            "longitude": -90.0715
+          }, {
+            "zoomLevel": 5,
+            "scale": 0.5,
+            "title": "Brisbane",
+            "latitude": -27.4698,
+            "longitude": 153.0251
+          }, {
+            "zoomLevel": 5,
+            "scale": 0.5,
+            "title": "Haarlem",
+            "latitude": 52.3874,
+            "longitude": 4.6462
+          }, {
+            "zoomLevel": 5,
+            "scale": 0.5,
+            "title": "Beijing",
+            "latitude": 39.9042,
+            "longitude": 116.4074
+          }, {
+            "zoomLevel": 5,
+            "scale": 0.5,
+            "title": "Seoul",
+            "latitude": 37.5665,
+            "longitude": 126.9780
+          }, {
+            "zoomLevel": 5,
+            "scale": 0.5,
+            "title": "Nancy",
+            "latitude": 48.6921,
+            "longitude": 6.1844
+          }, {
+            "zoomLevel": 5,
+            "scale": 0.5,
+            "title": "Amsterdam",
+            "latitude": 52.3702,
+            "longitude": 4.8952
+          }, {
+            "zoomLevel": 5,
+            "scale": 0.5,
+            "title": "Tokyo",
+            "latitude": 35.6785,
+            "longitude": 139.6823
+          }
+        ]
+    }
+  } );
+
+  // add events to recalculate map position when the map is moved or zoomed
+  map.addListener( "positionChanged", updateCustomMarkers );
+
+  // this function will take current images on the map and create HTML elements for them
+  function updateCustomMarkers( event ) {
+    // get map object
+    var map = event.chart;
+
+    // go through all of the images
+    for ( var x in map.dataProvider.images ) {
+      // get MapImage object
+      var image = map.dataProvider.images[ x ];
+
+      // check if it has corresponding HTML element
+      if ( 'undefined' == typeof image.externalElement )
+        image.externalElement = createCustomMarker( image );
+
+      // reposition the element accoridng to coordinates
+      var xy = map.coordinatesToStageXY( image.longitude, image.latitude );
+      image.externalElement.style.top = xy.y + 'px';
+      image.externalElement.style.left = xy.x + 'px';
+    }
+  }
+
+  // this function creates and returns a new marker element
+  function createCustomMarker( image ) {
+    // create holder
+    var holder = document.createElement( 'div' );
+    holder.className = 'map-marker';
+    holder.title = image.title;
+    holder.style.position = 'absolute';
+
+    // maybe add a link to it?
+    if ( undefined != image.url ) {
+      holder.onclick = function() {
+        window.location.href = image.url;
+      };
+      holder.className += ' map-clickable';
+    }
+
+    // create dot
+    var dot = document.createElement( 'div' );
+    dot.className = 'dot';
+    holder.appendChild( dot );
+
+    // create pulse
+    var pulse = document.createElement( 'div' );
+    pulse.className = 'pulse';
+    holder.appendChild( pulse );
+
+    // append the marker to the map container
+    image.chart.chartDiv.appendChild( holder );
+
+    return holder;
+  }
